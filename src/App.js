af75a8020c42bf894efd9b3907fac9aa798372af
Valeriy Ostrowerhow <valeriy22@web.de>
2024-04-01 17:14:56 +0200

    App+Home

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
-import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "/node_modules/bootstrap/dist/js/bootstrap.min.js";
+import "./App.css";
+import Home from "./Home";
+import StudentsView from "./component/client/ClientsView";
+import NavBar from "./component/common/NavBar";
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+} from "react-router-dom";
+import AddClient from "./component/student/AddClient";
+import EditClient from "./component/student/EditClient";
+import ClientPofile from "./component/student/ClientProfile";
 
 function App() {
-  return (
-    <div>
-    <h1>Hello</h1>
-    </div>
-  );
+	return (
+		<main className="container mt-5">
+			<Router>
+				<NavBar />
+				<Routes>
+					<Route
+						exact
+						path="/"
+						element={<Home />}></Route>
+					<Route
+						exact
+						path="/view-clients"
+						element={<ClientsView />}></Route>
+					<Route
+						exact
+						path="/add-clients"
+						element={<AddClient />}></Route>
+					<Route
+						exact
+						path="/edit-client/:id"
+						element={<EditClient />}></Route>
+					<Route
+						exact
+						path="/client-profile/:id"
+						element={<ClientProfile />}></Route>
+				</Routes>
+			</Router>
+		</main>
+	);
 }
 
 export default App;
